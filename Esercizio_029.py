e2503b2eac8715358b6e2866bbe3368b3e9dfb63
Simon Kola <147493438+SimonKolaa@users.noreply.github.com>
2024-01-16 17:38:21 +0100

    Esercizio_029.py

--- Esercizio_029.py
+++ Esercizio_029.py
@@ -4,25 +4,25 @@ def calcola_importo(fatture: list[dict]) -> list[float] | None:
     totale = 0
     totale_scontato = 0
     for fattura in fatture:
-        importo = fattura["importo"]
-        sconto = fattura["sconto_fattura"] / 100
-        importo_scontato = importo * (1 - sconto)
-        fattura["importo_scontato"] = importo_scontato
-        totale += importo
-        totale_scontato += importo_scontato
-    return [totale, totale_scontato]
+     importo = fattura["importo"]
+    sconto = fattura["sconto_fattura"] / 100
+    importo_scontato = importo * (1 - sconto)
+    fattura["importo_scontato"] = importo_scontato
+    totale += importo
+    totale_scontato += importo_scontato
+    return [totale,totale_scontato]
 
 
 fatture = [
 {"id": "Monticelli", "importo": 245.78, "sconto_fattura": 10},
 {"id": "Kola", "importo": 325.71, "sconto_fattura": 12},
-{"id": "Romagna", "importo": 245.78, "sconto_fattura": 8},
-{"id": "Bilancioni", "importo": 245.78, "sconto_fattura": 15},
-{"id": "Sanchi", "importo": 245.78, "sconto_fattura": 5},
+{"id": "Romagna_la_squale", "importo": 245.78, "sconto_fattura": 8},
+{"id": "Bilancioni2drugrich", "importo": 245.78, "sconto_fattura": 15},
+{"id": "Sanchi_la_rue", "importo": 245.78, "sconto_fattura": 5},
 {"id": "Pontellini", "importo": 245.78, "sconto_fattura": 18},
-{"id": "Clementi", "importo": 245.78, "sconto_fattura": 20},
+{"id": "Clementino_la_street", "importo": 245.78, "sconto_fattura": 20},
 {"id": "Arlotti", "importo": 245.78, "sconto_fattura": 19},
-{"id": "Nedria", "importo": 245.78, "sconto_fattura": 7},
+{"id": "Nedria_Mychajlo_Mudryk", "importo": 245.78, "sconto_fattura": 7},
 {"id": "Santini", "importo": 245.78, "sconto_fattura": 22},
 ]
 
@@ -32,4 +32,4 @@ print("Totale importi scontati:", risultati[1])
 
 
 
-print("il mancio non mi ha scammato")
+print("mancio ha soldi sporchi dentro zainetti LV")
